cc4220cd45a93370ed4d64a5270a85b71397d6cd
Dmitry Serov <barbari100@gmail.com>
2018-03-20 02:31:52 +0700

    Add user-mode sample using DeviceIoControl

--- mojom/generator/gene/main.cpp
+++ mojom/generator/gene/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+#include <windows.h>
+#include <strsafe.h>
+
 #include "test.mojom.h"
 #include "test.mojom.server.h"
 #include "gene_runtime.h"
@@ -6,6 +12,7 @@
 using namespace my_module;
 
 int main() {
+	/*
     MyStruct str = {
 		"hello worlc",
          0x1488,
@@ -40,6 +47,52 @@ int main() {
     if (r.ProcessIncomingMessage(container)) {
         std::cout << "Success receiving and processing msg" << std::endl;
     }
+	*/
+
+	HANDLE hDevice = CreateFile("\\\\.\\TEST",
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (hDevice == INVALID_HANDLE_VALUE) {
+		std::cout << "Error: CreateFile Failed : " << GetLastError() << std::endl;
+		return 7;
+	}
+
+	char OutputBuffer[100];
+	char InputBuffer[100];
+
+	if (FAILED(StringCchCopy(InputBuffer, sizeof(InputBuffer),
+		"this String is from User Application; using METHOD_BUFFERED"))) {
+		return 7;
+	}
+
+	std::cout << "Calling DeviceIoControl..." << std::endl;
+	memset(OutputBuffer, 0, sizeof(OutputBuffer));
+
+	ULONG bytesReturned;
+
+	BOOL bRc = DeviceIoControl(hDevice,
+		(DWORD)CTL_CODE(40001, 0x902, METHOD_BUFFERED, FILE_ANY_ACCESS),
+		InputBuffer,
+		(DWORD)strlen(InputBuffer) + 1,
+		OutputBuffer,
+		sizeof(OutputBuffer),
+		&bytesReturned,
+		NULL);
+
+	if (!bRc)
+	{
+		std::cout << "Error in DeviceIoControl : " << GetLastError() << std::endl;
+		return 7;
+
+	}
+	std::cout << "    OutBuffer (" << bytesReturned << "): " << OutputBuffer << std::endl;
+
+	CloseHandle(hDevice);
 
 
   return 0;
